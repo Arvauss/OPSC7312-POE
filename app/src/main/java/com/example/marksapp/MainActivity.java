7a9bd879545a259f825b94cf9cbf0114818ffce7
Dewald-10119314 <st10119314@vcconnect.edu.za>
2022-10-16 15:47:50 +0200

    MainMenu GoToMap button clicks MapActivity display current location on create

--- app/src/main/java/com/example/marksapp/MainActivity.java
+++ app/src/main/java/com/example/marksapp/MainActivity.java
@@ -1,57 +1,103 @@
 package com.example.marksapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static boolean isGpsEnabled = false;
-    public static double lat = 0, lng = 0;
 
+    public static boolean isGranted = false;
+
+    CardView MapCard, HomeCard, WorkCard;
+
+    @SuppressLint("MissingPermission")
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        isGpsEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        CheckPermissions();
+        InitUI();
 
-        // TODO: GETS USER'S CURRENT LOCATION, REQUIRES PERMISSION CHECK, REMOVE COMMENTS AFTER
-        /*if (!isGpsEnabled)
-        {
-            Intent enable_gps_intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(enable_gps_intent);
+
+    }
+
+
+
+    public void InitUI(){
+        MapCard = (CardView) findViewById(R.id.Mapcard_id);
+        HomeCard = (CardView) findViewById(R.id.Homecard_id);
+        WorkCard = (CardView) findViewById(R.id.Workcard_id);
+
+        MapCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent GoToMap = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(GoToMap);
+            }
+        });
+        HomeCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double hlat = 0, hlng = 0;
+                Intent GoToMapHome = new Intent(getApplicationContext(), MapsActivity.class);
+                GoToMapHome.putExtra("DestLat", hlat);
+                GoToMapHome.putExtra("DestLng", hlng);
+                startActivity(GoToMapHome);
+            }
+        });
+        WorkCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double wlat = 0, wlng = 0;
+                Intent GoToMapWork = new Intent(getApplicationContext(), MapsActivity.class);
+                GoToMapWork.putExtra("DestLat", wlat);
+                GoToMapWork.putExtra("DestLng", wlng);
+                startActivity(GoToMapWork);
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void CheckPermissions() {
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)){
+            isGranted = true;
         } else {
-            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 30, new LocationListener() {
-                @Override
-                public void onLocationChanged(@NonNull Location location) {
-                    lat = location.getLatitude();
-                    lng = location.getLongitude();
-                }
-                @Override
-                public void onProviderEnabled(@NonNull String provider) {
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        }
+    }
 
-                }
-
-                @Override
-                public void onProviderDisabled(@NonNull String provider) {
-
-                }
-
-                @Override
-                public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                }
-            });
-
-        }*/
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 0: if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+            break;
+            default:
+                Toast.makeText(this, "Permissions Not Granted", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/marksapp/MapsActivity.java
+++ app/src/main/java/com/example/marksapp/MapsActivity.java
@@ -1,8 +1,17 @@
 package com.example.marksapp;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,12 +26,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
 
+    public static boolean isGpsEnabled = false;
+    public static double lat = 0, lng = 0;
+    public static LatLng destLatLng;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Bundle dest = getIntent().getExtras();
+        destLatLng = new LatLng(dest.getDouble("DestLat"), dest.getDouble("DestLng"));
+        Toast.makeText(this, destLatLng.toString(), Toast.LENGTH_SHORT).show();
+
+
+
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -42,10 +63,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        GetLocation(mMap);
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+    @SuppressLint("MissingPermission")
+    public void GetLocation(GoogleMap mMap){
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        isGpsEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!isGpsEnabled)
+        {
+            Intent enable_gps_intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(enable_gps_intent);
+        } else {
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 30, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    lat = location.getLatitude();
+                    lng = location.getLongitude();
+                    // Toast.makeText(MainActivity.this, "Lat: " + lat + "\tLng: " + lng, Toast.LENGTH_SHORT).show();
+                    LatLng curLoc = new LatLng(lat,lng);
+                    mMap.addMarker(new MarkerOptions().position(curLoc).title("Current Location"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(curLoc));
+                }
+                @Override
+                public void onProviderEnabled(@NonNull String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(@NonNull String provider) {
+
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+            });
+
+        }
     }
 }
